67ea167782d41414cd1fb391014346872c7ba656
alextsu <alextsu@gmail.com>
2015-12-16 23:16:19 -0500

    first

---
+++ atsu-rss-reader/atsu-rss-reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  atsu-rss-reader
+//
+//  Created by Alexander Tsu on 12/16/15.
+//  Copyright (c) 2015 Alexander Tsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ atsu-rss-reader/atsu-rss-readerTests/atsu_rss_readerTests.swift
@@ -0,0 +1,36 @@
+//
+//  atsu_rss_readerTests.swift
+//  atsu-rss-readerTests
+//
+//  Created by Alexander Tsu on 12/16/15.
+//  Copyright (c) 2015 Alexander Tsu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class atsu_rss_readerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
